6e3d1d589779137571ccad8bc38166895b1002df
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-29 18:39:00 -0400

    footer and stylized

--- app/src/2header.js
+++ app/src/2header.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <div style={{ textAlign: 'center', padding: '20px' }}>
+        <div>
         <h1>The Filthy Mudbucket</h1>
-        <p>"We dont have crabs and don't check our health score!"</p>
+        <div style={{ textAlign: 'center', padding: '5px' }}>
+        <p>" We dont have crabs and don't check our health score!™ "</p>
         <p>7196 E. Ann Street</p>
         <p>Ringgold, GA 30736</p>
-        <p>(859)867-5309</p>
+        <p>(859) 867-5309</p>
+        <a href="#" style={{ fontSize: '200%' }}>Online Order!</a>
+        </div>
       </div>
     );
 }
