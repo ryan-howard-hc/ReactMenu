98988790494e9da56818bc3199bd077a0975bd71
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-29 18:07:37 -0400

    easy bootstrap for menu

--- app/src/1nav.js
+++ app/src/1nav.js
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-
-
+import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
+    <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px', background: '#f0f0f0' }}>
+      <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
+        <li style={{ marginRight: '20px' }}><a href="#">Home</a></li>
+        <li style={{ marginRight: '20px' }}><a href="#">About</a></li>
+        <li style={{ marginRight: '20px' }}><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
     </nav>
   );
-};
-
+}
