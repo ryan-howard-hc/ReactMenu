d3cac7b6cebd51986952c87b91ebb3c76317015e
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-30 10:52:08 -0400

    last update for codefreeze

--- app/src/5doordash.js
+++ app/src/5doordash.js
@@ -5,13 +5,12 @@ import ReactDOM from 'react-dom';
 export default function Doordash() {
     return (
         <div>
-
+            <input type="text" placeholder="Credit card info"></input>
         </div>
     );
 }
 
 const currentPath = window.location.pathname;
-if (currentPath === 'OrderOnline') {
+if (currentPath === '/OrderOnline') {
   ReactDOM.render(<Doordash />, document.getElementById('root'));
 }
-
