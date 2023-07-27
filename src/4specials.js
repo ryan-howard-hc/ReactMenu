e0d7d0310a51f32a46124afbab9f1193bf14c4fe
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-27 18:41:55 -0400

    yes

---
+++ src/4specials.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useEffect, useState} from 'react';
+import axios from 'axios';
+
+export default function Specials() {
+
+        const [menuItems, setMenuItems] = useState([]);
+        const [loading, setLoading] = useState(true);
+        useEffect(() => {
+            axios.get('http://127.0.0.1:8000/menu/get_menu/')
+              .then(response => {
+                let data = response.data;
+                setMenuItems(data);
+              })
+              .catch(error => {
+                console.error(error);
+              })
+              .finally(() => {
+                setLoading(false);              //prevented eternal loading...
+              });
+          }, []);
+
+    return (
+         <div id="specials">
+            <h1 style= {{ borderBottom: '1px solid #000', borderTop: '1px solid #000', padding: '10px 0' }}>Specials</h1>
+        {menuItems.slice(16, 24).map(item => (
+          <div className="card" key={item.id}>
+            <h3 className="card-title">{item.title}</h3>
+            <p className="card-text">{item.description}</p>
+            <p className="card-text">Price: {item.price}</p>
+            <p className="card-text">Spicy Level: {item.spicy_level}</p>
+          </div>
+        ))}
+      </div>
+    );
+}
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+// import axios from 'axios';
+
+import Menu from './0menu';
+// import Doordash from './5doordash';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+
+      <script src="/5doordash.js"></script>
+    </div>
+  );
+}
+
+export default App;
