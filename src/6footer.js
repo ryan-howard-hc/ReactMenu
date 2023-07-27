00392af59a5bfe55138f8596a24be1962ff4d7a9
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-27 18:57:41 -0400

    Revert "yes"
    This reverts commit e0d7d0310a51f32a46124afbab9f1193bf14c4fe.

--- src/6footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-    return (
-        <div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', backgroundColor: 'aquamarine', textAlign: 'center' , opacity: '0.75'}}>
-      <h1 style={{ margin: '0 auto', padding: '5px 0' , opacity: '1.0 !important'}}><a href="#">Online Order!</a></h1>
-        </div>
-    );
-}
